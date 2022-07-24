d0e0571cfa85042aa14d6ef7ab34baca5a936fb0
whitecastle20 <68242854+whitecastle20@users.noreply.github.com>
2022-07-24 19:49:33 +0900

    departmentXml commit

---
+++ app/src/main/java/kr/co/company/android16/FutureIndustrialConvergence.kt
@@ -0,0 +1,11 @@
+package kr.co.company.android16
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FutureIndustrialConvergence : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_future_industrial_convergence)
+    }
+}
---
+++ app/src/main/java/kr/co/company/android16/NaturalSciences.kt
@@ -0,0 +1,11 @@
+package kr.co.company.android16
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NaturalSciences : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_natural_sciences)
+    }
+}
