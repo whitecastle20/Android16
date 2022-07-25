f948ce38fdbb8553fec8b64ab58daa0975bf7956
yyyujinnn <1209yujin@naver.com>
2022-07-26 00:49:48 +0900

    Parameter commit

--- app/src/main/java/kr/co/company/android16/ArtDesign.kt
+++ app/src/main/java/kr/co/company/android16/ArtDesign.kt
@@ -2,10 +2,25 @@ package kr.co.company.android16
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.VisibilitySetterAction
+import android.widget.Button
 
 class ArtDesign : AppCompatActivity() {
+
+    // 변수 선언
+    lateinit var ContemporaryArt : Button
+    lateinit var CeramicsFiberArt : Button
+    lateinit var VisualCommunicationDesign : Button
+    lateinit var AdvancedMedia : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_art_design)
+
+        // 변수 연결
+        ContemporaryArt = findViewById(R.id.ContemporaryArt)
+        CeramicsFiberArt = findViewById(R.id.CeramicsFiberArt)
+        VisualCommunicationDesign = findViewById(R.id.VisualCommunicationDesign)
+        AdvancedMedia = findViewById(R.id.AdvancedMedia)
     }
 }
--- app/src/main/java/kr/co/company/android16/ChooseDepartment.kt
+++ app/src/main/java/kr/co/company/android16/ChooseDepartment.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 
 class ChooseDepartment : AppCompatActivity() {
 
+    // 변수 선언
     lateinit var btnHumanities: Button
     lateinit var btnSocialSciences: Button
     lateinit var btnNaturalSciences: Button
@@ -19,6 +20,7 @@ class ChooseDepartment : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_department)
 
+        // 변수 연결
         btnHumanities = findViewById(R.id.btnHumanities)
         btnSocialSciences = findViewById(R.id.btnSocialSciences)
         btnNaturalSciences = findViewById(R.id.btnNatualSciences)
@@ -26,23 +28,33 @@ class ChooseDepartment : AppCompatActivity() {
         btnArtDesign = findViewById(R.id.btnArtDesign)
         btnFreeMajor = findViewById(R.id.btnFreeMajor)
 
+        // 단과대 버튼 선택으로 학과 선택화면 이동
         btnHumanities.setOnClickListener {
             val intent = Intent(this, Humanities::class.java)
+            startActivities(intent)
         }
         btnSocialSciences.setOnClickListener {
             val intent = Intent(this, SocialSciences::class.java)
+            startActivities(intent)
         }
 
         btnNaturalSciences.setOnClickListener {
             val intent = Intent(this, NaturalSciences::class.java)
+            startActivities(intent)
         }
 
         btnFutureIndustrialConvergence.setOnClickListener {
             val intent = Intent(this, FutureIndustrialConvergence::class.java)
+            startActivities(intent)
         }
 
         btnArtDesign.setOnClickListener {
             val intent = Intent(this, ArtDesign::class.java)
+            startActivities(intent)
         }
+    }
+
+    private fun startActivities(intent: Intent) {
+
     }
 }
